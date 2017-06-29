b328099ece893dd458474639ac75de72d652f024
Stian Bakkane <stianba@gmail.com>
2017-06-29 13:05:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const port = process.env.PORT || 1337;
 const app = express();
 
 app.set("view engine", "pug");
-
 app.use("/article", article);
-
 app.get("/", (req, res) => res.json({ message: "Hey ho" }));
 
 app.listen(port);
