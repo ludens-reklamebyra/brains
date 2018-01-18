7524ae3257d527abcac797936064f9e62b6f5a3b
oyvindhermansen <oyvind@ludensreklame.no>
2018-01-18 16:39:08 +0100

    release

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const apicache = require('apicache');
 const work = require('./routes/work');
 const article = require('./routes/article');
 const pages = require('./routes/pages');
+const compression = require('compression');
 require('dotenv').config();
+
+const cache = apicache.middleware;
 
 const port = process.getuid() || 1337;
 //const port = 1337;
 const app = express();
 
 app.set('view engine', 'pug');
+app.use(compression());
+app.use(cache('1 hour'));
 app.use(express.static('public'));
 app.use('/jobber', work);
 app.use('/artikkel', article);
