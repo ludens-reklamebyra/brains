323b537e7caafec216b52d5d0b62a8ceeb6dd5c6
Øyvind Hermansen <oyvindhermansen@yvinds-MacBook-Pro.local>
2019-02-18 14:37:37 +0100

    added caching again

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.use(compression());
-// app.use(cache('1 hour'));
+app.use(cache('1 hour'));
 app.use(express.static('public'));
 app.use('/jobber', work);
 app.use('/aktuelt', article);
