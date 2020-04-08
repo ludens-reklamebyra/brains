1bbd2e3172169f78bcbc8c0113a6e4cc70a54890
Øyvind Hermansen <oyvindhermansen@yvinds-MacBook-Pro.local>
2020-04-08 15:10:47 +0200

    added redirect

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
 const express = require('express');
-const apicache = require('apicache');
-const work = require('./routes/work');
-const article = require('./routes/article');
-const pages = require('./routes/pages');
-const service = require('./routes/service');
-const compression = require('compression');
 require('dotenv').config();
 
-const cache = apicache.middleware;
-const devPort = 1337;
+//const devPort = 4000;
 const prodPort = process.getuid();
 
 // Remember to change to prodPort when deploying.
 const port = prodPort;
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(compression());
-app.use(cache('1 hour'));
-app.use(express.static('public'));
-app.use('/jobber', work);
-app.use('/aktuelt', article);
-app.use('/blogg', article);
-app.use('/tjenester', service);
-app.get('*', pages);
+app.get('*', (req, res) => {
+  res.redirect('https://ludensgruppen.no' + req.path);
+});
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
